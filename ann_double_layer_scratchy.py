d6e96394d333122526b91b281a68e5321ab7a143
Pascal Brouqui <pascal.brouqui@gmail.com>
2017-01-17 02:21:05 +0100

    Add files via upload

---
+++ ann_double_layer_scratchy.py
@@ -0,0 +1,55 @@
+# import
+import numpy as np
+
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+
+    return 1/(1+np.exp(-x))
+    
+# input 
+X = np.array([[0,0,1],
+             [0,1,1],
+             [1,0,1],
+             [1,1,1]])
+    
+# output             
+y = np.array([[0]
+             ,[1]
+             ,[1]
+             ,[0]])
+
+# seed random 
+np.random.seed(1)
+
+# weights initialization
+syn0 = 2*np.random.random((3,4)) - 1
+syn1 = 2*np.random.random((4,1)) - 1
+
+for j in xrange(60000):
+
+    # forward prop
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+    l2 = nonlin(np.dot(l1,syn1))
+    
+    # l2 error
+    l2_error = y - l2
+    
+    if (j% 10000) == 0:
+        print "Error:" + str(np.mean(np.abs(l2_error)))
+
+    # back prop
+    l2_delta = l2_error * nonlin(l2,deriv=True)
+    
+    # l1 value to l2 error
+    l1_error = l2_delta.dot(syn1.T)
+    
+    # back prop
+    l1_delta = l1_error * nonlin(l1,deriv=True)
+    
+
+    # weights update
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += l0.T.dot(l1_delta)
